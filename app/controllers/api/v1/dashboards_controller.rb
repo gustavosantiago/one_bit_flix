1155becf0ce1bd1595a1d4c552b091d9aab3ae06
Gustavo Santiago <gustavosantig1@gmail.com>
2019-08-26 04:33:15 -0300

    include controller rules and actions

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -1,4 +1,26 @@
 class Api::V1::DashboardsController < ApplicationController
+  WHITELIST = %i(category keep_watching highlight).freeze
+
   def index
+    type_params and (return if performed?)
+
+    result = DashboardService.new(params[:type], current_user).perform
+
+    render json: result
+  end
+
+  private
+
+  def type_params
+    params[:type] ||= "category"
+    type_whitelist
+  end
+
+  def type_whitelist
+    unless WHITELIST.include?(params[:type])
+
+      render json: {
+        error: "Unpermitted type parameter"
+      }, status: :forbidden
   end
 end
--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::MoviesController < ApplicationController
+  before_action :set_movie, only: :show
+
   def show
+    render json: Api::V1::MovieSerializer.new(
+      @movie, include: [:episodes],
+      params: { user: current_user }
+    ).serialized_json
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find_by(id: params[:id])
   end
 end
